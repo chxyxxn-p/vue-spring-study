9f47347f39d280eb9700fdb2aa96cc59278d39be
Choyeon Park <chxyxxn.p@gmail.com>
2025-07-13 14:06:02 +0900

    initial commit : 프로젝트 기본 구조

---
+++ backend/src/main/java/com/cyp/study/VueSpringStudyApplication.java
@@ -0,0 +1,13 @@
+package com.cyp.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VueSpringStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VueSpringStudyApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/cyp/study/VueSpringStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cyp.study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VueSpringStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
