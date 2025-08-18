68e503adbc151e8e85f47c5f52e32ef60e9c72c5
Choyeon Park <chxyxxn.p@gmail.com>
2025-08-18 22:54:47 +0900

    - backend url 설정 파일 추가(dev(local), prod)  - GET 호출시 설정 파일에서 url 읽어오도록 수정

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,7 +1,8 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
+
 module.exports = defineConfig({
   transpileDependencies: true,
-  devServer: {
-    port: 3000,
+  devServer: {  //개발 서버에서만 적용
+    port: 3000,  //vue 개발 서버 포트
   }
 })
